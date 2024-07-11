fd2f40a690be769b89b0f6781a41d77ccf93cab0
ismaahmadmad <ismaa7990@gmail.com>
2024-07-11 16:20:29 +0500

    CURD operations with classes

--- controller/data_controller.js
+++ controller/data_controller.js
@@ -1,27 +1,32 @@
-const service = require("../services/data_services");
+const Info = require("../services/data_services");
+// console.log("Service object:", service);
 
 class InfoController {
- 
+ constructor(){
+    this.service = new Info();
+    this.addInfo = this.addInfo.bind(this);
+    this.getInfo = this.getInfo.bind(this);
+    this.getInfoById = this.getInfoById.bind(this);
+    this.updateInfo = this.updateInfo.bind(this);
+    this.deleteInfo = this.deleteInfo.bind(this);
+    // console.log("Service object:", this.service);
+ }
  async addInfo(req, res) {
     const { id, first_name, last_name, email } = req.body;
-    const addedInfo = await service.addInfo(id, first_name, last_name, email)
-    res.json(addedInfo.rows);
-   
+    const addedInfo = await this.service.addInfo(id, first_name, last_name, email);
+    return res.json(addedInfo.rows);
   }
 
  async getInfo(req, res) {
-   const get = await service.getInfo()
-      
-    res.json(get.rows);
-       
-      
+   const get = await this.service.getInfo(); 
+   return res.json(get.rows);   
   }
 
  async getInfoById(req, res) {
     const id = req.params.id;
 
-   const result = await service.getInfoById(id)
-    res.json(result.rows);
+   const result = await this.service.getInfoById(id)
+   return res.json(result.rows);
        
   }
 
@@ -29,15 +34,15 @@ class InfoController {
     const id = req.params.id;
     const { first_name, last_name, email } = req.body;
   
-    const updatedData = await service.updateInfo(id, first_name, last_name, email);
-    res.json(updatedData.rows);
+    const updatedData = await this.service.updateInfo(id, first_name, last_name, email);
+    return res.json(updatedData.rows);
   }
 
   async deleteInfo(req, res) {
     const id = req.params.id;
 
-    const data = await service.deleteInfo(id)
-    res.send("data deleted successfully..!!");
+    const data = await this.service.deleteInfo(id)
+    return res.send("data deleted successfully..!!");
      
   }
 }
--- services/data_services.js
+++ services/data_services.js
@@ -34,4 +34,4 @@ class Info {
   }
 }
 
-module.exports = new Info();
+module.exports = Info;
